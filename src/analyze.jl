760b2d0643c7b0c96082ed53b52905b031778406
Uwe Fechner <fechner@aenarete.eu>
2021-11-21 20:17:11 +0100

    Started to implement selling

--- src/analyze.jl
+++ src/analyze.jl
@@ -1,6 +1,9 @@
 using CSV, DataFrames, Plotly, Dates, TimeZones, Impute
 
 logfiles=["log_1637352719.csv","log_1637489560.csv"]
+INDEX = 1
+MAX_TRADE = 100.0 # max EUR per trade when buying
+FEE = 1.0 - 0.45/100.0 # 0.45% fee per trade (0.25 fee, 0.2% spread)
 
 # fetch the latest log file from the server
 function fetch_log()
@@ -14,16 +17,6 @@ function seconds2human(delta)
     minutes = div(reminder, 60)
     seconds = mod(reminder, 60) 
     return Dates.Hour(hours) + Dates.Minute(minutes) + Dates.Second(seconds)
-end
-
-function fill_missing(df)
-    t0 = first(df.TIME)
-    t_end = last(df.TIME)
-    t=t0
-    while t <= t_end
-        println(t)
-        t+=60
-    end 
 end
 
 function read_log()
@@ -121,7 +114,7 @@ function overview(df)
         push!(DROP_1h, drop_1h(df, Symbol(name)))
         push!(DROP_24h, drop_24h(df, Symbol(name)))
     end
-    res = DataFrame(NAME = names(df), RISE_1h = CHANGES_1h, DROP_1h = DROP_1h, RISE_24h = CHANGES_24h, DROP_24h = DROP_24h)
+    res = DataFrame(MARKET = names(df), RISE_1h = CHANGES_1h, DROP_1h = DROP_1h, RISE_24h = CHANGES_24h, DROP_24h = DROP_24h)
     delete!(res, 1) # delete time entry
     res_hour = sort!(res, [:RISE_1h, :RISE_24h], rev=true)
     by_hour = first(res_hour, 8)
@@ -130,8 +123,115 @@ function overview(df)
     return by_hour, by_day
 end
 
+function trade_db(df, save_eur::Float64)
+    # time, market, sell_eur, buy_eur, sell_coins, buy_coins, save_eur, withdraw_eur, total
+    global INDEX
+    t0 = first(df.TIME)
+    INDEX=1
+    trade_db = DataFrame(TIME=t0, MARKET = "DEPOSIT", SELL_EUR=0.0, BUY_EUR=0.0, SELL_COINS=0.0, BUY_COINS=0.0, SAVE_EUR=save_eur, WITHDRAW_EUR=0.0, TOTAL=save_eur)
+end
+
+function calc_cash(df, tdb)
+    cash = 0.0
+    for row in eachrow(tdb)
+        market1 = row.MARKET
+        if market1=="DEPOSIT"
+            cash += row.SAVE_EUR - row.WITHDRAW_EUR
+        else
+            rate1 = last(df[!, market1])
+            cash -= (row.BUY_EUR - row.SELL_EUR)
+        end
+    end    
+    cash
+end
+
+function calc_total(df, tdb)
+    total = 0.0
+    for row in eachrow(tdb)
+        market = row.MARKET
+        if market=="DEPOSIT"
+            total+=row.SAVE_EUR - row.WITHDRAW_EUR
+        else
+            rate = last(df[!, market])
+            total+=(row.BUY_COINS - row.SELL_COINS) * rate
+        end
+    end
+    return total
+end
+
+function buy(df, tdb, time, market, amount)
+    global FEE
+    subset = filter(row -> row.MARKET == market, tdb)
+    old_amount = sum(subset.BUY_EUR) - sum(subset.SELL_EUR)
+    cash = calc_cash(df, tdb)
+    if old_amount < 0.01 && cash >= amount
+        rate = last(df[!, market])
+
+        println("Buy: ", market, " rate: ", rate)
+        coins = amount / rate * FEE
+        total = calc_total(df, tdb) - amount + coins * rate
+        v = [time, market, 0.0, amount, 0.0, coins, 0.0, 0.0, total]
+        push!(tdb, v)
+    end
+end
+
+# sell all coins of a given market
+function sell(df, tdb, time, market)
+    subset = filter(row -> row.MARKET == market, tdb)
+    old_amount = sum(subset.BUY_COINS) - sum(subset.SELL_COINS)
+    rate = last(df[!, market])
+    println("Sell: ", market, " coins: ", old_amount, " rate: ", rate)
+    sell_eur = old_amount * rate
+    total = calc_total(df, tdb) + sell_eur
+    v = [time, market, sell_eur, 0.0, old_amount, 0.0, 0.0, 0.0, total]
+    push!(tdb, v)
+end
+
+function sell_all(df, tdb)
+    markets = ["MLN_EUR", "ALICE_EUR"]
+    time = last(df.TIME)
+    for market in markets
+        sell(df, tdb, time, market)
+    end
+end
+
+function check(df, tdb)
+    global INDEX, MAX_TRADE
+    # create view to db with the first INDEX rows
+    view = df[1:INDEX, :]
+    by_hour, by_day = overview(view)
+    # println(by_hour[1,:])
+    for row in eachrow(by_hour)
+        if row.RISE_1h >= 5.0
+            market = row.MARKET
+            time = df.TIME[INDEX] + 10          
+            buy(view, tdb, time, market, MAX_TRADE)
+        end
+    end
+    INDEX+=1
+end
+
+function trade(df, n=0)
+    if n == 0
+        n = size(df)[1]
+    end
+    tdb = trade_db(df, 1000.0)
+    for i in 1:n
+        check(df, tdb)
+    end
+    tdb
+end
+
 df = read_log()
-by_hour, by_day = overview(df)
-println(by_hour)
-println(by_day)
+if true
+    by_hour, by_day = overview(df)
+    println(by_hour)
+    println(by_day)
+else
+    trade(df, 1000)
+end
+
+# create trade data base
+# tdb = trade_db(df, 1000.0)
+# check(df, tdb)
 # p1 = plot(df.BTC_EUR, label="BTC_EUR")
