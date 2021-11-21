a4a54630ae39eb58515fa284b536acd887fa6774
Uwe Fechner <fechner@aenarete.eu>
2021-11-21 13:39:04 +0100

    add drop calculation

--- src/analyze.jl
+++ src/analyze.jl
@@ -74,6 +74,22 @@ function change_1h(df, name)
     change = (current/min - 1.0) * 100.0
 end
 
+function drop_1h(df, name)
+    col     = df[!, name]
+    window = col[max((length(col)-60+1), 1):end]
+    max1     = maximum(window)
+    current = last(col)
+    drop = (current/max1 - 1.0) * 100.0
+end
+
+function drop_24h(df, name)
+    col     = df[!, name]
+    window = col[max((length(col)-24*60+1), 1):end]
+    max1     = maximum(window)
+    current = last(col)
+    drop = (current/max1 - 1.0) * 100.0
+end
+
 function plot_1h(df, name)
     col     = df[!, name]
     window = col[max((length(col)-60+1), 1):end]
@@ -97,16 +113,20 @@ end
 function overview(df)
     CHANGES_1h = Float64[]
     CHANGES_24h = Float64[]
+    DROP_1h = Float64[]
+    DROP_24h = Float64[]
     for name in names(df)
         push!(CHANGES_1h,  change_1h(df, Symbol(name)))
         push!(CHANGES_24h, change_24h(df, Symbol(name)))
+        push!(DROP_1h, drop_1h(df, Symbol(name)))
+        push!(DROP_24h, drop_24h(df, Symbol(name)))
     end
-    res = DataFrame(NAME = names(df), CHANGE_1h = CHANGES_1h, CHANGE_24h = CHANGES_24h)
+    res = DataFrame(NAME = names(df), RISE_1h = CHANGES_1h, DROP_1h = DROP_1h, RISE_24h = CHANGES_24h, DROP_24h = DROP_24h)
     delete!(res, 1) # delete time entry
-    res_hour = sort!(res, [:CHANGE_1h, :CHANGE_24h], rev=true)
-    by_hour = first(res_hour, 5)
-    res_day = sort!(res, [:CHANGE_24h, :CHANGE_1h], rev=true)
-    by_day  = first(res_day, 5)
+    res_hour = sort!(res, [:RISE_1h, :RISE_24h], rev=true)
+    by_hour = first(res_hour, 8)
+    res_day = sort!(res, [:RISE_24h, :RISE_1h], rev=true)
+    by_day  = first(res_day, 8)
     return by_hour, by_day
 end
 
