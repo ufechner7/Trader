dbfc99d1c6bf07fcbdbfb32e57dec7f9435d5d28
Uwe Fechner <fechner@aenarete.eu>
2021-11-25 13:47:25 +0100

    improved performance of test_3b, now both tests show 190% per month

--- src/analyze.jl
+++ src/analyze.jl
@@ -14,7 +14,7 @@ MIN_DROP      = -25.0            # sell if DROP_1h goes below this value [%]
 MIN_DROP_24   = -35.0            # sell if DROP_24h goes below this value [%]
 WAIT          = 60               # number of minutes to wait before dealing
 DAYS          = 4                # number of days to wait for valid rating
-MIN_RATING    = 100               # minimal rating to buy a coin
+MIN_RATING    = 125              # minimal rating to buy a coin
 T0            = 0
 rating_tab    = nothing
 
@@ -505,7 +505,13 @@ function test2(df)
 end
 
 function test3(df, plot=false)
-    tdb=trade(df)
+    global TDB
+    if isnothing(TDB)
+        tdb = trade(df, 0)
+        TDB=tdb
+    else
+        tdb=TDB
+    end
     markets = list_markets(tdb)
     sell_all(df, tdb, markets)
 
