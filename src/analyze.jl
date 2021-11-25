a7d0a65f78cd7ff33150287dd85d5ee5b7bbf84f
Uwe Fechner <fechner@aenarete.eu>
2021-11-25 15:24:41 +0100

    performance now 303% for first test

--- src/analyze.jl
+++ src/analyze.jl
@@ -14,7 +14,7 @@ MIN_DROP      = -25.0            # sell if DROP_1h goes below this value [%]
 MIN_DROP_24   = -35.0            # sell if DROP_24h goes below this value [%]
 WAIT          = 60               # number of minutes to wait before dealing
 DAYS          = 4                # number of days to wait for valid rating
-MIN_RATING    = 100              # minimal rating to buy a coin
+MIN_RATING    = 75              # minimal rating to buy a coin
 T0            = 0
 rating_tab    = nothing
 
