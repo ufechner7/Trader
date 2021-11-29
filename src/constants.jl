552c32af31f7fb5d97c06425a88ddd90639fe863
Uwe Fechner <fechner@aenarete.eu>
2021-11-29 23:11:39 +0100

    all const

--- src/constants.jl
+++ src/constants.jl
@@ -3,19 +3,19 @@
 # scalar constants
 const START_KAPITAL = 1000.0           # in EUR
 const MAX_TRADE     = 140.0            # max EUR per trade when buying
-KEEP                = 0.0            # min EUR to keep as cash
+const KEEP          = 0.0            # min EUR to keep as cash
 const FEE           = 1.0 - 0.45/100.0 # 0.45% fee per trade (0.25 fee, 0.2% spread)
 const MAX_RISE      =  4.4             # buy  if RISE_1h goes above this value [%]
 const MIN_DROP      = -25.0            # sell if DROP_1h goes below this value [%]
 const MIN_DROP_24   = -35.0            # sell if DROP_24h goes below this value [%]
 const DAYS          = 4                # number of days to wait for valid rating
-MIN_RATING          = 25.0             # minimal rating to buy a coin; 
-MAX_RATING          = 15.0
-EXTRA_RATING        = 0.0              # extra rating for fast rise was: 12.5
-TAU                 = 180              # time constant for decay of extra rating
-INTERVAL            = 6                # intervall between evaluations in hours
-STOP_LIMIT          = 0.85             # if total drops below STOP_LIMIT * maximum, then STOP
-ALLOW_RATING        = false
+const MIN_RATING    = 25.0             # minimal rating to buy a coin; 
+const MAX_RATING    = 15.0
+const EXTRA_RATING        = 0.0              # extra rating for fast rise was: 12.5
+const TAU                 = 180              # time constant for decay of extra rating
+const INTERVAL            = 6                # intervall between evaluations in hours
+const STOP_LIMIT          = 0.85             # if total drops below STOP_LIMIT * maximum, then STOP
+const ALLOW_RATING        = false
 
 # array constants
 PREFER = String[]
