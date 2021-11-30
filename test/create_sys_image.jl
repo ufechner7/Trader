6c36e9c0fdbe920d6a3256052216411b440894dc
Uwe Fechner <fechner@aenarete.eu>
2021-11-30 21:53:58 +0100

    update precompilation script and package list

--- test/create_sys_image.jl
+++ test/create_sys_image.jl
@@ -1,12 +1,12 @@
 @info "Loading packages ..."
-using FluxArchitectures, Plots, CSV, DataFrames, Plotly, TimeZones
+using FluxArchitectures, Plots, CSV, DataFrames, TimeZones, Dates, TimeZones, Impute, Statistics, GLM, Parameters, JLD2
 using PackageCompiler
 
 @info "Creating sysimage ..."
 push!(LOAD_PATH,joinpath(pwd(),"src"))
 
 PackageCompiler.create_sysimage(
-    [:FluxArchitectures, :Plots, :CSV, :DataFrames, :Plotly, :TimeZones];
+    [:FluxArchitectures, :Plots, :CSV, :DataFrames, :TimeZones, :Impute, :Statistics, :GLM, :Parameters, :JLD2];
     sysimage_path="MakieSys_tmp.so",
     precompile_execution_file=joinpath("test", "test_for_precompile.jl")
 )
--- test/test_for_precompile.jl
+++ test/test_for_precompile.jl
@@ -33,7 +33,7 @@ Flux.train!(loss, Flux.params(model),Iterators.repeated((input, target), 2), ADA
 @info "Final loss" loss = loss(input, target)
 
 println("-------------------------------------")
-using CSV, DataFrames, Plotly, Dates, TimeZones
+using CSV, DataFrames, Dates, TimeZones, Impute
 
 # fetch the latest log file from the server
 function fetch_log()
@@ -49,14 +49,42 @@ function seconds2human(delta)
     return Dates.Hour(hours) + Dates.Minute(minutes) + Dates.Second(seconds)
 end
 
-function read_log()
-    df = CSV.read("data/log_1637352719.csv", DataFrame)
-    new_names=Symbol[]
-    i = 1
-    for header in names(df)
-        push!(new_names, Symbol(replace(header, "-" => "_")))
+function read_log(logfiles; df = nothing)   
+    t_end = 0
+    if ! isnothing(df)
+        t_end = last(df.TIME)
     end
-    rename!(df, new_names)
+    for logfile in logfiles
+        df_new = CSV.read("data/" * logfile, DataFrame; ntasks=1)
+        new_names=Symbol[]
+        for header in names(df_new)
+            push!(new_names, Symbol(replace(header, "-" => "_")))
+        end
+        rename!(df_new, new_names)
+        if isnothing(df)
+            df=df_new
+            t_end = last(df.TIME)
+        else
+            t_start = first(df_new.TIME)
+            if (t_start - t_end) > 60
+               n = div(t_start - t_end + 30, 60)
+               v = fill(missing, size(df)[2]-1)
+               println("Missing: ", n, " minutes") 
+               allowmissing!(df)
+               for i in 1:n
+                   v1 = vcat([i*60+t_end], v)
+                   push!(df, v1)
+               end
+            end
+            df = outerjoin(df, df_new, matchmissing=:equal, on = intersect(names(df),  names(df_new)))
+            t_end = last(df.TIME)
+        end
+    end
+
+    df = Impute.interp(df)
+    disallowmissing!(df)
+
+
     data_length = last(df.TIME) - first(df.TIME)
     utc_time = unix2datetime(last(df.TIME))
     local_time = ZonedDateTime(utc_time, TimeZone("Europe/Amsterdam"); from_utc=true) 
@@ -109,7 +137,7 @@ function overview(df)
     return by_hour, by_day
 end
 
-df = read_log()
+df = read_log(["log_1637352719.csv"])
 by_hour, by_day = overview(df)
 println(by_hour)
 println(by_day)
