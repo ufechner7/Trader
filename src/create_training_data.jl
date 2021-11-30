8c4f7e7e62fa9862685e11138575a3e6875a8014
Uwe Fechner <fechner@aenarete.eu>
2021-12-01 00:50:03 +0100

    many fixes

--- src/create_training_data.jl
+++ src/create_training_data.jl
@@ -1,23 +1,26 @@
 PREDICT = 24   # number of hours to predict
 MINRISE = 10.0 # min rise over PREDICT hours to trigger buying 
-MARKETS = ["LTO_EUR","AVAX_EUR","ATA_EUR","CHR_EUR","MANA_EUR","SAND_EUR"]
+MARKETS = ["LTO_EUR","AVAX_EUR","ATA_EUR","CHR_EUR","MANA_EUR","SAND_EUR", "ALICE_EUR", "ROSE_EUR", "LRC_EUR", "SHIB_EUR", "COTI_EUR", "DOT_EUR"]
 
 # TODO split the data in real and fake data (good and bad data sets)
 
+using JLD2, DataFrames
+
 function create_training_db(st)
     df = nothing
-    for st.index in 1:(size(st.df)[1] - PREDICT*60)
+    for st.index in 60:(size(st.df)[1] - PREDICT*60)
         view = st.df[1:st.index, :]
         future_view = st.df[st.index+1:st.index + PREDICT*60-1, :]
-        for market in MARKETS
+        markets = first(names(st.df)[2:end], 30)
+        for market in markets
             # println(market)
             time = view.TIME[st.index]
             rise1h = rise_1h(view, market)
             rise24h = rise_24h(view, market)
-            future_rise24h = future_view[!, market][end] - future_view[!, market][1]
+            future_rise24h = ((future_view[!, market][end] / future_view[!, market][1]) - 1.0) * 100.0
             buy = future_rise24h > MINRISE
             if isnothing(df)
-                df = DataFrame(TIME=time, MARKET=market, RISE_1h=rise1h, RISE_24h=rise24h, FUTURE_RISE_24=future_rise24h, BUY=buy)
+                df = DataFrame(TIME=time, MARKET=market, RISE_1h=Float64(rise1h), RISE_24h=Float64(rise24h), FUTURE_RISE_24=Float64(future_rise24h), BUY=buy)
             else
                 v = [time, market, rise1h, rise24h, future_rise24h, buy]
                 push!(df, v) 
