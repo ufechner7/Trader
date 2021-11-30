51c064bbbc1d6c734df713c0cd91930eab3f3f25
Uwe Fechner <fechner@aenarete.eu>
2021-11-30 11:29:25 +0100

    add runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,34 @@
+using BenchmarkTools, Test, DataFrames, CSV, Dates, TimeZones, Impute, Parameters
+
+include("../src/utils.jl")
+include("../src/logging.jl")
+include("../src/basic.jl")
+include("../src/tests.jl")
+
+if ! @isdefined st
+    st = State(read_log(logfiles()))
+end
+if isnothing(st.tdb)
+    include("../src/analyze.jl")
+    test3(st)
+end
+
+function calc_cash1(tdb::DataFrame)
+    cash::Float64 = 0.0
+    for row in eachrow(tdb)
+        market = row.MARKET
+        if market=="DEPOSIT"
+            cash += row.SAVE_EUR - row.WITHDRAW_EUR
+        elseif market != ""
+            cash -= (row.BUY_EUR - row.SELL_EUR)
+        end
+    end    
+    cash
+end
+
+# buy(st, view, rp_table, market, amount; force=false, reason="")
+
+@test calc_cash(st.tdb) == calc_cash1(st.tdb)
+
+@btime calc_cash($st.tdb)  #  1.7 μs
+@btime calc_cash1($st.tdb) # 37.6 μs
