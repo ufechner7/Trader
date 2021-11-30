c0ff6d1a23e26c5b86695c82ae349b7b876195f3
Uwe Fechner <fechner@aenarete.eu>
2021-11-30 08:51:21 +0100

    use PyPlot in precompile

--- test/create_sys_image.jl
+++ test/create_sys_image.jl
@@ -1,12 +1,12 @@
 @info "Loading packages ..."
-using FluxArchitectures, Plots, CSV, DataFrames, Plotly, TimeZones
+using FluxArchitectures, Plots, CSV, DataFrames, PyPlot, TimeZones
 using PackageCompiler
 
 @info "Creating sysimage ..."
 push!(LOAD_PATH,joinpath(pwd(),"src"))
 
 PackageCompiler.create_sysimage(
-    [:FluxArchitectures, :Plots, :CSV, :DataFrames, :Plotly, :TimeZones];
+    [:FluxArchitectures, :Plots, :CSV, :DataFrames, :PyPlot, :TimeZones];
     sysimage_path="MakieSys_tmp.so",
     precompile_execution_file=joinpath("test", "test_for_precompile.jl")
 )
