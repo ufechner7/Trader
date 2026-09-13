7143727bcd28af3a6b3e214e03a161056d850d1e
Uwe Fechner <fechner@aenarete.eu>
2026-09-13 15:55:13 +0200

    Update create_sys_image

--- test/create_sys_image.jl
+++ test/create_sys_image.jl
@@ -1,12 +1,13 @@
 @info "Loading packages ..."
-using FluxArchitectures, Plots, CSV, DataFrames, TimeZones, Dates, TimeZones, Impute, Statistics, GLM, Parameters, JLD2
+using TimeZones, Dates, FluxArchitectures
 using PackageCompiler
 
+TimeZones.build()
+
 @info "Creating sysimage ..."
-push!(LOAD_PATH,joinpath(pwd(),"src"))
 
 PackageCompiler.create_sysimage(
-    [:FluxArchitectures, :Plots, :CSV, :DataFrames, :TimeZones, :Impute, :Statistics, :GLM, :Parameters, :JLD2];
+    [:TimeZones, :FluxArchitectures];
     sysimage_path="MakieSys_tmp.so",
     precompile_execution_file=joinpath("test", "test_for_precompile.jl")
 )
